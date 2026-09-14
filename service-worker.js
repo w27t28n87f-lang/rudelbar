938f185dc1530ea66d9a519bf6898ccc126e0126
w27t28n87f-lang <w27t28n87f@privaterelay.appleid.com>
2026-09-14 12:27:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "rudelbar-app-cache-v147";
+const CACHE_NAME = "rudelbar-app-cache-v148";
 
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=147",
-  "./app.js?v=147",
-  "./manifest.json?v=147",
+  "./style.css?v=148",
+  "./app.js?v=148",
+  "./manifest.json?v=148",
   "./Logo-Haupt.png",
   "./Logo-Haupt-Home.png",
   "./Logo-Mobile_Kneipe.png",
